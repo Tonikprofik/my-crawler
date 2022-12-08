7e48ec41b0653fedc717b59c0eb9d29aed0b443b
PonikTrofik <izanami369@gmail.com>
2022-12-08 21:45:31 +0100

    feat:phone viewport

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,39 @@
 import { PlaywrightCrawler, ProxyConfiguration } from "crawlee";
 import { router } from "./routes.js";
 
-const startUrls = ["https://bookingportal.com/wannasport-test-courtee.dev"];
+const startUrls = ["https://bookingportal.com/wannasport-test-court"];
 
 const crawler = new PlaywrightCrawler({
   // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
-  requestHandler: router,
+  requestHandler: async ({ request, page, log }) => {
+    await page.setViewportSize({ width: 390, height: 800 });
+    // desktop .available
+    // await page.waitForSelector(".available");
+    // const available = await page.locator(".available").count();
+    // log.info(`available: ${available}`);
+
+    await page.waitForSelector(
+      "#main > div > div > div > div.main-area.col-md-9 > div.subview-host > div > div.resource-booking-info > div > div > div.grid-container > div > div > div > div:nth-child(3) > div"
+    );
+    const infoEl = await page
+      .locator(
+        "#main > div > div > div > div.main-area.col-md-9 > div.subview-host > div > div.resource-booking-info > div > div > div.grid-container > div > div > div > div:nth-child(3) > div"
+      )
+      .count();
+    log.info(`infoEl: ${infoEl}`);
+
+    const mobileInfoEl = await page.locator(".mobile > .info-element").count();
+    log.info(`mobileInfoEl: ${mobileInfoEl}`);
+
+    // const passedSlots = await page.locator(".passed").count();
+    // const availableSlots = available - passedSlots;
+    log.info(
+      //   `availble ${available} - passedSlots ${passedSlots}= availableSlots ${availableSlots}`
+      ``
+    );
+  },
+  headless: true,
+  maxRequestsPerCrawl: 20,
 });
 
 await crawler.run(startUrls);
--- src/routes.ts
+++ src/routes.ts
@@ -1,17 +1,22 @@
-import { Dataset, createPlaywrightRouter } from "crawlee";
+import { Dataset, createPlaywrightRouter, Configuration } from "crawlee";
 
 export const router = createPlaywrightRouter();
 
-router.addDefaultHandler(async ({ enqueueLinks, log }) => {
+// const config = Configuration.getGlobalConfig();
+// config.set("headless", false);
+
+router.addDefaultHandler(async ({ log }) => {
   log.info(`enqueueing new URLs`);
-  await enqueueLinks({
-    globs: ["https://bookingportal.com/wannasport-test-court"],
-    label: "detail",
-  });
+  //   await enqueueLinks({
+  //     globs: ["https://bookingportal.com/wannasport-test-court/"],
+  //     label: "detail",
+  //   });
 });
 
 router.addHandler("detail", async ({ request, page, log }) => {
+  log.info(`available`);
   const available = await page.locator(".available").count();
+  log.info(`available: ${available}`);
   const passedSlots = await page.locator(".passed").count();
   const availableSlots = available - passedSlots;
 
