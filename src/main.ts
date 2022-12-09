3069cc965ecf00e9590b685b9079a08214befc1a
PonikTrofik <izanami369@gmail.com>
2022-12-09 17:48:24 +0100

    log info elements current day and next day

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 // For more information, see https://crawlee.dev/
-import { PlaywrightCrawler, ProxyConfiguration } from "crawlee";
+import { Dataset, PlaywrightCrawler, ProxyConfiguration } from "crawlee";
 import { router } from "./routes.js";
 
 const startUrls = ["https://bookingportal.com/wannasport-test-court"];
@@ -16,24 +16,65 @@ const crawler = new PlaywrightCrawler({
     await page.waitForSelector(
       "#main > div > div > div > div.main-area.col-md-9 > div.subview-host > div > div.resource-booking-info > div > div > div.grid-container > div > div > div > div:nth-child(3) > div"
     );
-    const infoEl = await page
-      .locator(
-        "#main > div > div > div > div.main-area.col-md-9 > div.subview-host > div > div.resource-booking-info > div > div > div.grid-container > div > div > div > div:nth-child(3) > div"
-      )
-      .count();
-    log.info(`infoEl: ${infoEl}`);
 
-    const mobileInfoEl = await page.locator(".mobile > .info-element").count();
-    log.info(`mobileInfoEl: ${mobileInfoEl}`);
+    const countPassed = await page.locator("div.past").count();
+    log.info(`countPassed: ${countPassed} `);
+
+    const countInfoEl = await page
+      .locator(".day, .info-element >> visible = true")
+      .count();
+    // 15
+    // >> visible = true, 7 
+    log.info(`countInfoEl: ${countInfoEl} `);
+
+    //all text
+    let dataInfoElText: any = [];
+    const alltext = async () => {
+      // show if its not class past
+      for (let i = 0; i < countInfoEl; ++i) {
+        //if ( await page.locator(".day, .info-element >> visible = true").nth(i).)) {
+
+          const text = await page
+          .locator(".day, .info-element >> visible = true")
+          .nth(i)
+          .textContent();
+          log.info(`text: ${text} `);
+          // append to object dataInfoElText
+          await dataInfoElText.push(text);
+       // }
+      }
+    };
+    await alltext();
+
+    await page
+      .locator(".day, .info-element >> visible = true")
+      .filter({ hasText: "Klik for at se" })
+      .first()
+      .click();
+
+    await alltext();
+
+    // const infoEl = await page.locator(".day, .info-element");
+    // // for each infoEl with .day, .info-element class get textContent
+    // await infoEl.filter({ hasText: "Klik for at se" }).first().click();
+
+    // const slotsofDay = await page.waitForSelector(".slots");
+    // const locatSlotsOfDay = await page.locator(".slots").first();
+    // const countSlotsOfDay = await locatSlotsOfDay.filter({ hasText: "Ledig" }).count();
+
+    // log.info(`slots: ${slotsofDay} locatSlotsOfDay: ${locatSlotsOfDay} countSlotsOfDay: ${countSlotsOfDay} `);
 
     // const passedSlots = await page.locator(".passed").count();
     // const availableSlots = available - passedSlots;
-    log.info(
-      //   `availble ${available} - passedSlots ${passedSlots}= availableSlots ${availableSlots}`
-      ``
-    );
+
+    await Dataset.pushData({
+      countInfoEl,
+      dataInfoElText,
+      url: request.loadedUrl,
+    });
   },
   headless: true,
+
   maxRequestsPerCrawl: 20,
 });
 
